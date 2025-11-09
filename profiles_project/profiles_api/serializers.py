92a5c6c92e3eb1c394cec709504c7e109c294294
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-11-09 12:47:59 +0530

    user registration, login, update user data with security, toker header

---
+++ profiles_project/profiles_api/serializers.py
@@ -0,0 +1,31 @@
+from . models import UserProfile
+from rest_framework import serializers
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializer a user profile object"""
+
+    password = serializers.CharField(write_only=True,
+                                     style={'input_type':'password'},
+                                     min_length = 8)
+
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'email', 'name', 'password']
+    
+    def create(self, validated_data):
+        user = UserProfile.objects.create_user(
+            validated_data['email'],
+            validated_data['name'],
+            validated_data['password']
+        )
+        return 
+    
+    def update(self, instance, validated_data):
+        """Handle updating user password senario"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from . models import UserProfile
+from . serializers import UserProfileSerializer
+
+from rest_framework.authentication import TokenAuthentication
+from . permissions import UpdateOwnProfile
+from rest_framework import filters
+
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
+
+
+
+class UserProfileViewset(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+    # even we logged-in,need to pass Token Header
+    authentication_classes = [TokenAuthentication]
+
+    permission_classes = [UpdateOwnProfile]
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'email']
+
+
+
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
